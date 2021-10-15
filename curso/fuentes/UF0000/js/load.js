0c206bee3b3c962dc951da77df5b7a829a4ade3f
unknown <scabelloj@innovacionycualificacion.com>
2021-10-15 11:16:31 +0200

    actualizar

--- curso/fuentes/UF0000/js/load.js
+++ curso/fuentes/UF0000/js/load.js
@@ -1,8 +1,33 @@
-var url             = '../0000comunes/comunes_v12/',
-    urlLoadDinamic  = url + 'js/loadDinamic.js';
+let produccion = "../0000comunes/comunes_v12/",
+		desarrollo = "https://recursosadicionales.com/desarrollo/0000comunes/comunes_v12/",
+		url = produccion,
+		urlLoadDinamic = url + 'js/loadDinamic.js';
 
-let head = document.getElementsByTagName('head')[0],
-    element = document.createElement('script');
-element.type = 'text/javascript';
-element.src = urlLoadDinamic;
-head.appendChild(element);
+const req = new XMLHttpRequest();
+req.open('GET', urlLoadDinamic);
+req.send();
+
+// Comprobar que estamos en producción o trabajando en programación
+req.onloadend = () => {
+
+	// Si no estamos en producción o programación cambiamos a desarrollo (maquetación)
+	if (req.status == 404) {
+		url = desarrollo;
+		urlLoadDinamic = url + 'js/loadDinamic.js';
+	}
+
+	// Insertar el script que nos enlaza con el motor de la plantilla
+	let head = document.getElementsByTagName('head')[0],
+  element = document.createElement('script');
+	element.type = 'text/javascript';
+	element.src = url + 'js/loadDinamic.js';
+	element.defer = true;
+	head.appendChild(element);
+
+	// Mensaje informativo
+	if (urlLoadDinamic.search('recursosadicionales') >= 0)
+		console.log("MODO DESARROLLO");
+	else if (window.location.host.search('.local') >= 0)
+		console.info("MODO PROGRAMACIÓN");
+	else console.info("MODO PRODUCCIÓN");
+}
